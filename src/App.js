097340bff0708b6ad697efed3d66c790c2f4d231
Hafsah <hafsa.muhammad999@gmail.com>
2021-08-09 19:40:56 +0500

    Transition Component added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-
+import { Transition } from "react-transition-group";
 import "./App.css";
 import Modal from "./components/Modal/Modal";
 import Backdrop from "./components/Backdrop/Backdrop";
@@ -7,22 +7,55 @@ import List from "./components/List/List";
 
 class App extends Component {
   state = {
-    modalIsOpen: false
-  }
+    modalIsOpen: false,
+    showBlock: false,
+  };
 
   openModal = () => {
-    this.setState({modalIsOpen:true})
-  }
+    this.setState({ modalIsOpen: true });
+  };
   closeModal = () => {
-    this.setState({modalIsOpen:false})
-  }
+    this.setState({ modalIsOpen: false });
+  };
   render() {
     return (
       <div className="App">
         <h1>React Animations</h1>
-        <Modal show={this.state.modalIsOpen} closed={this.closeModal}/>
-        <Backdrop show={this.state.modalIsOpen}/>
-        <button className="Button" onClick={this.openModal}>Open Modal</button>
+        <button
+          className="Button"
+          onClick={() =>
+            this.setState((prevState) => ({ showBlock: !prevState.showBlock }))
+          }
+        >
+          Toggle
+        </button>
+        <br />
+        <Transition
+          in={this.state.showBlock}
+          timeout={1000}
+          mountOnEnter
+          unmountOnExit
+        >
+          {(state) => (
+            <div
+              style={{
+                backgroundColor: "red",
+                height: 100,
+                width: 100,
+                margin: "auto",
+                transition: "opacity 1s ease-out",
+                opacity: state === "exiting" ? 0 : 1,
+              }}
+            ></div>
+          )}
+        </Transition>
+        <Modal show={this.state.modalIsOpen} closed={this.closeModal} />
+        {this.state.modalIsOpen ? (
+          <Backdrop show={this.state.modalIsOpen} />
+        ) : null}
+        <button className="Button" onClick={this.openModal}>
+          Open Modal
+        </button>
         <h3>Animating Lists</h3>
         <List />
       </div>
